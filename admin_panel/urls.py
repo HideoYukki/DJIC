2709c4a59f66db500f3aaf841ab99c1d87a991f2
HideoYukki <dominik0414@hotmail.com>
2026-06-22 12:16:39 +0200

    Delete admin_panel directory

--- admin_panel/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    path('logs/', views.logs_list),
-    path('logs/export/', views.logs_export),
-    path('settings/', views.settings_view),
-]
