421d4a14f49278996e331a1c9c7a8e58e86ed264
HideoYukki <dominik0414@hotmail.com>
2026-06-22 12:17:35 +0200

    Delete notifications directory

--- notifications/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    path('', views.notification_list),
-    path('<str:notification_id>/read/', views.mark_read),
-    path('read-all/', views.mark_all_read),
-]
